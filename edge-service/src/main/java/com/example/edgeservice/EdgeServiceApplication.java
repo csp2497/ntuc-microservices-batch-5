5caa3bcaef79c092cb8d4c791766888230f0598a
Mukesh Kumar <mukeshkumar@Mukesh-Kumars-MacBook-Pro.local>
2019-11-26 11:07:11 +0800

    Adding the hystrix and dashboard.

--- edge-service/src/main/java/com/example/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/example/edgeservice/EdgeServiceApplication.java
@@ -1,13 +1,18 @@
 package com.example.edgeservice;
 
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 public class EdgeServiceApplication {
 
 	public static void main(String[] args) {
