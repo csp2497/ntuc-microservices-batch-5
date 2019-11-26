fb62ad75bee1528168548d0e959ea72d03ddf450
Mukesh Kumar <mukeshkumar@Mukesh-Kumars-MacBook-Pro.local>
2019-11-26 10:02:14 +0800

    Adding the edge service

---
+++ edge-service/src/main/java/com/example/edgeservice/AuthFilter.java
@@ -0,0 +1,38 @@
+package com.example.edgeservice;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+//@Component
+public class AuthFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String authorization = request.getHeader("Authorization");
+        if(authorization == null){
+            ctx.setResponseStatusCode(401);
+            ctx.setSendZuulResponse(false);
+        }
+        return null;
+    }
+}
