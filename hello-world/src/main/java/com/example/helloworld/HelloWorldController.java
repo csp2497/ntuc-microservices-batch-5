ff51beb9c21cf27ce99809136571419b36502733
Mukesh Kumar <mukeshkumar@Mukesh-Kumars-MacBook-Pro.local>
2019-11-26 08:33:40 +0800

    Adding the configuration change.

--- hello-world/src/main/java/com/example/helloworld/HelloWorldController.java
+++ hello-world/src/main/java/com/example/helloworld/HelloWorldController.java
@@ -1,14 +1,20 @@
 package com.example.helloworld;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HelloWorldController {
+
+    @Value("${greeting}")
+    private String greeting;
 
     @GetMapping("/hello-world/{name}")
     public String hello(@PathVariable String name){
-        return "Hello " + name + " !!";
+        return greeting + " "+ name + " !!";
     }
 }
