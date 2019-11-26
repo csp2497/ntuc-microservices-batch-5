7d2719644ba5f9258d04b0fc57a28fbbc2d1348d
Mukesh Kumar <mukeshkumar@Mukesh-Kumars-MacBook-Pro.local>
2019-11-26 14:16:51 +0800

    Adding the Zipkin, sleth deps for distributed tracing.

--- item-service/src/main/java/com/example/itemservice/ItemController.java
+++ item-service/src/main/java/com/example/itemservice/ItemController.java
@@ -1,21 +1,37 @@
 package com.example.itemservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 @RestController
+@Slf4j
 public class ItemController {
 
     @Value("${server.port}")
     private String port;
+    private int errorCounter = 0;
+
 
     @GetMapping("/items")
-    public List<String> getItems(){
-        System.out.println("Calling the item service...");
-        return Arrays.asList("iPhone", "Samsung", "Nokia", port);
+    public List<String> getItems() throws InterruptedException {
+        log.info("getItem is invoked....");
+        Random random = new Random();
+        int low = 100;
+        int high = 1000;
+        int result = random.nextInt(high - low) + low;
+        Thread.sleep(result);
+        errorCounter++;
+        if(errorCounter%2==0){
+            log.error("Exception occurred in time service!!!!");
+            new RuntimeException("Exception occurred in time service!!!!");
+        }
+        log.info("getItem invocation is complete.");
+        return Arrays.asList("iphone", "samsung", "nokia", port);
     }
 }
--- order-service/src/main/java/com/example/orderservice/OrderControler.java
+++ order-service/src/main/java/com/example/orderservice/OrderControler.java
@@ -1,5 +1,6 @@
 package com.example.orderservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,14 +11,20 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class OrderControler {
 
     @Autowired
     public RestTemplate restTemplate;
 
-    @GetMapping("/orders/{orderId}")
-    public String getOrder(@PathVariable String orderId){
-        return orderId;
+    @GetMapping("/orders")
+    public String getOrder(){
+        try {
+            restTemplate.getForObject("http://ITEM-SERVICE/items", List.class);
+        }catch (Exception e){
+            log.error("Error occurred while calling the item service", e);
+        }
+        return "orderid-1";
     }
 
     @PostMapping("/orders")
